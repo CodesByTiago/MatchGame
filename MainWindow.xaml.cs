2a8c5e42cf937bc1296ecb561f2fe77adc3a112a
Tiago Correia <tiagocorreiia@gmail.com>
2025-01-28 15:28:48 -0300

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MatchGame;
+
+/// <summary>
+///     Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        SetUpGame();
+    }
+
+    private void SetUpGame()
+    {
+        List<string> animalEmoji = new List<string>
+        {
+            "🦑", "🦑",
+            "🐠", "🐠",
+            "🐘", "🐘",
+            "🐳", "🐳",
+            "🐪", "🐪",
+            "🦖", "🦖",
+            "🦘", "🦘",
+            "🦔", "🦔"
+        };
+
+        var random = new Random();
+
+        foreach (TextBlock textBlock in MainGrid.Children.OfType<TextBlock>())
+        {
+            int index = random.Next(animalEmoji.Count);
+            string nextEmoji = animalEmoji[index];
+            textBlock.Text = nextEmoji;
+            animalEmoji.RemoveAt(index);
+        }
+    }
+}
